891b68267d094c45b94668052c9099f475fd9ea2
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2025-07-20 15:51:42 -0400

    rewrite scraper.py

--- scraper.py
+++ scraper.py
@@ -4,200 +4,273 @@ from bs4 import BeautifulSoup
 import re
 import os
 from io import StringIO
-import time
 import logging
-import csv
-from typing import List, Dict
-from utils import *
+import json
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename=os.path.curdir + "/scraper.log", format='%(asctime)s %(message)s', encoding='utf-8', level=logging.INFO)
+logging.basicConfig(
+    filename=os.path.curdir + "/scraper.log",
+    format="%(asctime)s %(message)s",
+    encoding="utf-8",
+    level=logging.INFO,
+)
+
 
 class Scraper:
-    def __init__(self):
-        self.base_url = 'https://crs.cookcountyclerkil.gov'
-        self.url_templates = {
-        "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DateRecorded&direction=desc",
-        "https://crs.cookcountyclerkil.gov/Search/SortResultByPin?id1={pin}&column=DateRecorded&direction=asc",
-        "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=AlphaDocNumber&direction=desc",
-        "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=AlphaDocNumber&direction=asc",
-        "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DateExecuted&direction=desc",
-        "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DateExecuted&direction=asc",
-        "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DocTypeDescription&direction=desc",
-        "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DocTypeDescription&direction=asc"
-        }
-        self.data = {}
-        self.data_dir = os.getcwd() + '/data'
-        self.metadata_dir = self.data_dir + '/doc_metadata.csv'
 
-        # initialize data directories
+    def __init__(self):
+        self.base_url: str = "https://crs.cookcountyclerkil.gov"
+        self.url_templates: list = [
+            "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DateRecorded&direction=desc",
+            "https://crs.cookcountyclerkil.gov/Search/SortResultByPin?id1={pin}&column=DateRecorded&direction=asc",
+            "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=AlphaDocNumber&direction=desc",
+            "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=AlphaDocNumber&direction=asc",
+            "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DateExecuted&direction=desc",
+            "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DateExecuted&direction=asc",
+            "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DocTypeDescription&direction=desc",
+            "https://crs.cookcountyclerkil.gov/search/SortResultByPin?id1={pin}&column=DocTypeDescription&direction=asc",
+        ]
+        self.data: dict = {}
+        self.data_dir: str = os.getcwd() + "/data"
+
+    def make_snake_case(self, s: str) -> str:
+        """Converts a given string to snake_case
+
+        Parameters:
+            s (str): An input string
+
+        Returns:
+            A string in snake_case
+        """
+        s = str(s)
+        s = [char if char != " " else "_" for char in s]
+        return "".join(s).lower()
+
+    def initialize_data_directory(self):
+        """
+        Ensures that the data directory exists within the project
+        """
         if not os.path.exists(self.data_dir):
             logger.info("Initializing data directory")
             os.makedirs(self.data_dir)
 
-        if not os.path.exists(self.metadata_dir):
-            headers = ['document_number','document_type','date_recorded','date_executed','#_of_pages','address','consideration_amount', 'pin', 'download_url','path']
-            with open(self.metadata_dir, 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerow(headers)
-
-    def _clean_pin(self, pin: str) -> str:
-        '''Cleans a Property Identification Number (PIN) value so that it can be used in the base URLs.
+    def remove_duplicates(self, urls: list[str]) -> list[str]:
+        """Removes duplicate values from a list object.
 
         Parameters:
-            pin (str): An input PIN
+            urls (List[str]): A list of strings.
+
+        Returns:
+            A list of unique strings.
+        """
+        urls = list(set(urls))
+        return urls
+
+    def clean_pin(self, pin: str) -> str:
+        """
+        Cleans a Property Identification Number (PIN) value
+
+        Parameters:
+            pin (str): A formatted PIN
 
         Returns:
             A PIN without hyphens.
-        '''
+        """
         assert isinstance(pin, str), "pin must be of type str"
         pin = "".join(filter(str.isdigit, pin))
-        assert len(pin) == 14, "pin value must evaluate to a string of numeric digits of length 14"
+        assert (
+            len(pin) == 14
+        ), f"pin value must evaluate to a string of  digits of length 14, instead have {pin}"
         return pin
 
-    def get_pin_docs(self, pin: str):
-        '''Runs the scraping process for a given PIN.
-
-        Parameters:
-            pin (str): A PIN to scrape data for.
-
-        Returns:
-            None
-        '''
-        clean_pin = self._clean_pin(pin)
-        logger.info(f"Scraping for {clean_pin}...")
-        self.initialize_pin(clean_pin)
-        self.get_doc_view_links(clean_pin)
-        self.get_documents(clean_pin)
-    
     def initialize_pin(self, pin: str):
-        '''Initializes the PIN in the Scraper object, adding the given PIN value to the data.
+        """
+        Create an entry in the data structure for a given pin
+        """
+        # TODO: Add validation of PIN by checking website
+        clean_pin = self.clean_pin(pin)
+        self.data[clean_pin] = {"docs": {}, "properties": {}}
 
-        Parameters:
-            pin (str): The PIN to initialize.
+    def collect_doc_metadata(self, pin: str) -> list[dict[str]]:
+        """
+        Collects all of the available URls to document pages associated with a given PIN
+        """
+        def process_records(row: list) -> list:
+            # I'm not sure why the records returned from read_html are
+            # structured like this...
+            row = [val[0] if val[0] != "View" else self.base_url + val[1]
+                   for val in row]
+            return row
 
-        Returns:
-            None
-        '''
-        # TODO: Verify PIN exists on website
-        self.data[pin] = {'doc_urls': [], 'documents': {}}
-
-    def get_doc_view_links(self, pin: str) -> List[str]:
-        '''Collects and saves a collection of all URLs related to a given PIN on the Cook County Recorder web portal.
-
-        Parameters:
-            pin (str): A PIN value.
-
-        Returns:
-            collected_urls (List[str]): A List of URLs.
-        '''
-        collected_urls = []
+        doc_metadata = pd.DataFrame()
+        expected_nrows = 0  # TODO: This may not be necessary
         for url in self.url_templates:
-            logger.info("Querying " + url.format(pin = pin))
-            response = requests.get(url.format(pin = pin))
-            df = pd.read_html(StringIO(response.text), extract_links='body')[0] # TODO: Why is this a list?
-            view_urls = df['View Doc'].apply(lambda x: self.base_url + x[1]).to_list()
-            collected_urls.append(view_urls)
-        
-        collected_urls = [item for sublist in collected_urls for item in sublist]
-        collected_urls = remove_duplicates(collected_urls)
-        self.data[pin]['doc_urls'] = collected_urls
-        logger.info(str(len(collected_urls)) + " document urls collected")
-        return collected_urls
+            logger.info("Querying " + url.format(pin=pin))
+            response = requests.get(url.format(pin=pin))
+            df = pd.read_html(StringIO(response.text), extract_links="body")[0]
+            df = df.apply(lambda x: process_records(x))
+            df.rename(lambda x:
+                      self.make_snake_case(x),
+                      inplace=True, axis='columns')
+            expected_nrows += df.shape[0]
+            doc_metadata = pd.concat([doc_metadata, df])
 
-    def extract_doc_metadata(self, soup: BeautifulSoup, pin: str = "", url: str = "") -> Dict:
-        '''Extracts and normalizes document metadata for a given webpage on the Recorder web portal.
+        actual_nrows = doc_metadata.shape[0]
+        assert actual_nrows == expected_nrows, f"Actual pre-deduplication  \
+        records collected ({actual_nrows}) differs from \
+        expected ({expected_nrows})."
 
-        Parameters:
-            soup (BeautifulSoup): A BeautifulSoup object
-            pin (str): A PIN value
-            url (str): The URL to a document page on the Recorder web portal
+        doc_metadata = doc_metadata.drop_duplicates()
+        doc_metadata = doc_metadata.drop("unnamed:_0", axis=1)
+        doc_metadata = doc_metadata.rename({'view_doc': 'url'},
+                                           axis='columns')
+        doc_metadata = doc_metadata.to_dict(orient='records')
+        return doc_metadata
+
+    def extract_from_doc_urls(self):
+        '''
+        Iterate through the doc_urls collected and download the documents and
+        other relevant metadata.
+        '''
+        for pin in self.data:
+            for doc in self.data[pin]['docs']:
+                response = requests.get(self.data[pin]['docs'][doc]['url'])
+                soup = BeautifulSoup(response.text, features='lxml')
+                tag = soup.find("a", href=re.compile("/Document/DisplayPdf"))
+                pdf_url = self.base_url + tag.get("href")
+                self.data[pin]['docs'][doc]['pdf_url'] = pdf_url
+                self.download_pdf(pdf_url, pin, doc)
+
+                info_table = soup.fieldset.table.find_all('tr')
+                self.extract_info_table(info_table, pin, doc)
+
+                entities = self.extract_grantor_grantee_tables(soup)
+                self.data[pin]['docs'][doc]['entities'] = entities
+
+    def extract_grantor_grantee_tables(self, soup: BeautifulSoup):
+        """
+        Extracts Grantor and Grantee table data from Cook County Clerk's
+        document HTML.
 
         Returns:
-            A Dict containing metadata for the document found at the given URL 
-        '''
-        if "Error" in soup.title.string:
-            logger.error(f"bad doc url for {pin}, {url}")
-            return None
-        metadata = {}
-        doc_info_table = soup.fieldset.table.find_all("tr")
-        for record in doc_info_table:
-            key = record.th.label.string.strip(":")
-            key = make_snake_case(key)
-            value = record.td.string
-            metadata[key] = value
-        if not metadata.get('consideration_amount'): # some docs don't have this field
-            metadata['consideration_amount'] = ''
-        metadata['pin'] = pin
-        # TODO: metadata is missing grantor and grantee information found on same page at given url
-        logger.info(f"Successfully extracted metadata for {pin}, document {metadata['document_number']}")
-        return metadata
+            dict: Dictionary containing 'grantors' and 'grantees' lists
+            with extracted data
+        """
+        def extract_table_data(section_title):
+            """Helper function to extract data from a specific table section"""
+            # Find the span with the section title
+            section_span = soup.find(
+                'span', string=section_title, class_='fs-5')
+            if not section_span:
+                return []
 
-    def _save_metadata(self, metadata: Dict):
-        '''A helper function to save the metadata to a CSV file.
-        
+            # Find the table that follows this span
+            table = section_span.find_next('table', class_='table')
+            if not table:
+                return []
+
+            # Extract data from table rows (skip header row)
+            data = []
+            tbody = table.find('tbody')
+            if tbody:
+                rows = tbody.find_all('tr')
+                for row in rows:
+                    cells = row.find_all('td')
+                    if len(cells) >= 2:  # Ensure we have at least Name and Trust# columns
+                        # Extract name (could be a link or plain text)
+                        name_cell = cells[0]
+                        name_link = name_cell.find('a')
+                        name = name_link.get_text(
+                            strip=True) if name_link else name_cell.get_text(strip=True)
+
+                        # Extract trust number
+                        trust_num = cells[1].get_text(strip=True)
+
+                        data.append({
+                            'name': name,
+                            'trust_number': trust_num if trust_num else None
+                        })
+
+            return data
+
+        # Extract both tables
+        grantors = extract_table_data('Grantors')
+        grantees = extract_table_data('Grantees')
+
+        return {
+            'grantors': grantors,
+            'grantees': grantees
+        }
+
+    def download_pdf(self, url: str, pin: str, doc_num: str):
+        """
+        Downloads the document PDF to the data directory
+
         Parameters:
-            metadata (Dict): A dictionary of metadata for a PIN.
+            url (str): The URL containing the document.
+            pin (str): The PIN associated with the document.
+            doc_num (str): The document's ID number.
 
         Returns:
             None
-        '''
-        # TODO: Check that the files being written aren't already in the docs_metadata.csv file, right now the CSV is getting appended with duplicates if you run the Scraper on the same property
-        df = pd.DataFrame(metadata, index=[metadata['document_number']])
-        df.to_csv(self.metadata_dir, mode='a', header=False, index=False)
-
-    def get_documents(self, pin: str):
-        '''Accesses each URL in self.data[pin]['doc_urls'], extracts all metadata from the page, downloads the document PDF, and saves all of the data to the data directory.
-
-        Parameters:
-            pin (str): A PIN string.
-
-        Returns:
-            None
-        '''
-        assert self.data.get(pin).get('doc_urls'), "pin must have pulled view links to pull pdf links. run get_doc_view_links(pin) first."
-        
-        for url in self.data[pin]['doc_urls']:
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, features="lxml")
-            tag = soup.find('a', href=re.compile("/Document/DisplayPdf"))
-            pdf_url = self.base_url + tag.get('href')
-            # Collect all metadata
-            metadata = self.extract_doc_metadata(soup, pin, url)
-            metadata['download_url'] = pdf_url
-            metadata['path'] = self.data_dir + f"/{pin}/{metadata['document_number']}.pdf"
-            # Save metadata and download the PDF of the document
-            if not self.data[pin]['documents'].get(metadata['document_number']):
-                self.data[pin]['documents'][metadata['document_number']] = metadata
-                self.download_pdf(metadata['download_url'], metadata['path'])
-            else:
-                logger.info(f"Duplicate document at {url}, skipping...")
-            self._save_metadata(metadata)
-
-    def download_pdf(self, url: str, path: os.PathLike):
-        '''Downloads the document PDF from the given URL and saves it to the given path.
-
-        Parameters:
-            url (str): The URL containing the document. 
-            path (PathLike): The path to save the document to.
-
-        Returns:
-            None
-        '''
+        """
+        path = self.data_dir + "/" + pin + "/" + doc_num + '.pdf'
         if not os.path.exists(os.path.dirname(path)):
             os.makedirs(os.path.dirname(path))
-            logger.info(f"Created new directory for pin at {os.path.dirname(path)}")
+            logger.info(f"Created new directory for pin at {
+                        os.path.dirname(path)}")
         if os.path.exists(path):
             logger.info(f"{path} already exists, skipping")
-            return None
-        
+            return
+
         logger.info(f"Downloading file at {url}...")
         response = requests.get(url)
-        with open(path, 'wb') as file:
+        with open(path, "wb") as file:
             file.write(response.content)
-        assert os.path.exists(path), logger.error(f"Download was not successful to {path}")
-            
+            self.data[pin]['docs'][doc_num]['pdf_path'] = path
+        assert os.path.exists(path), logger.error(
+            f"Download from {url} was not successful to {path}"
+        )
+        logger.info(f"Document saved to {path}")
+
+    def extract_info_table(self,
+                           info_table: BeautifulSoup,
+                           pin: str,
+                           doc: str):
+        '''
+        Traverse across info_table and extract any metadata that
+        wasn't extracted during the metadata pull.
+        '''
+        for record in info_table:
+            key = record.th.label.string.strip(":")
+            key = self.make_snake_case(key)
+            value = record.td.string
+            if not self.data[pin]['docs'][doc].get(key):
+                key = 'doc_page_' + key
+                self.data[pin]['docs'][doc][key] = value
+                # TODO: This creates duplicate data entires
+
+    def main(self):
+        self.initialize_data_directory()
+
+        # pin = "16-10-421-053-0000" # Hotel Guyon
+        # pin = "17-05-115-085-0000"  # Starsiak Clothing
+        pin = "17-29-304-001-0000"
+        pin = self.clean_pin(pin)
+        self.initialize_pin(pin)
+        pin_doc_metadata = self.collect_doc_metadata(pin)
+
+        for doc in pin_doc_metadata:
+            doc_num = doc.pop('doc_number', None)
+            self.data[pin]['docs'][doc_num] = doc
+
+        self.extract_from_doc_urls()
+
+        out_path = self.data_dir + '/metadata.json'
+        with open(out_path, 'w') as file:
+            json.dump(self.data, file, indent=4, sort_keys=True)
+
+
 if __name__ == "__main__":
-    pin_to_pull = "16-10-421-053-0000" # Hotel Guyon
-    scraper = Scraper()
-    scraper.get_pin_docs(pin_to_pull)
+    s = Scraper()
+    s.main()
